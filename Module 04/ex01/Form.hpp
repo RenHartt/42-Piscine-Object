a96c29f99233bbff754e0b90f94d2d30da8957a0
Baptiste Goron <bgoron@3G3.42angouleme.fr>
2025-02-27 18:57:05 +0100

    change ptr into smart ptr

---
+++ Module 04/ex01/Form.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+enum class FormType
+{
+	CourseFinished,
+	NeedMoreClassRoom,
+	NeedCourseCreation,
+	SubscriptionToCourse
+};
+
+class Form
+{
+private:
+	FormType _formType;
+
+public:
+	Form(FormType p_formType)
+	{
+
+	}
+	virtual ~Form();
+
+	virtual void execute() = 0;
+};
+
+class CourseFinishedForm : public Form
+{
+private:
+
+public:
+	void execute();
+};
+
+class NeedMoreClassRoomForm : public Form
+{
+private:
+
+public:
+	void execute();
+};
+
+class NeedCourseCreationForm : public Form
+{
+private:
+
+public:
+	void execute();
+};
+
+class SubscriptionToCourseForm : public Form
+{
+private:
+
+public:
+	void execute();
+};
