12f78a6ef5063c2b2634eaa7c1cdc162145975dc
Baptiste Goron <bgoron@3G1.42angouleme.fr>
2025-03-28 16:46:46 +0100

    maj Module 02

--- Module 03/ex01/command.hpp
+++ Module 03/ex01/command.hpp
@@ -46,4 +46,18 @@ class Command {
             }
             return total;
         }
+
+        friend std::ostream& operator<<(std::ostream& os, const Command& command);
 };
+
+std::ostream& operator<<(std::ostream& os, const Command& command) {
+    os << "Commande " << command.id 
+    << " du " << command.date.date.tm_mday << "/" << command.date.date.tm_mon + 1 << "/" << command.date.date.tm_year + 1900
+    << " pour " << command.client.name << ":" << std::endl;
+    for (const auto& article : command.articles) {
+        os << "  - " << article.name << " (" << article.quantity << "x" << article.price << "€)\n";
+    }
+    os << "Prix total: " << command.get_total_price() << "€";
+
+    return os;
+}
--- Module 03/ex01/main.cpp
+++ Module 03/ex01/main.cpp
@@ -3,33 +3,29 @@
 #include "thuesdayDiscountCommand.hpp"
 #include "packageReductionDiscountCommand.hpp"
 
-void print_total_price(const Command& command) {
-    std::cout << "Total price: " << command.get_total_price() << " euros" << std::endl;
-}
-
 int main() {
     Client client("John Doe");
 
     std::set<Article> articles = {
-        Article("Laptop", 1000.0, 1),  // 1 laptop à 1000€
-        Article("Mouse", 50.0, 2)      // 2 souris à 50€ chacune
+        Article("Laptop", 1000.0, 1),
+        Article("Mouse", 50.0, 2)
     };
 
     Command base_order(1, client, articles);
-    std::cout << "Commande standard: ";
-    print_total_price(base_order); // Attendu: 1100€
+    std::cout << "Commande standard: " << std::endl;
+    std::cout << base_order << std::endl;
 
     ThuesdayDiscountCommand tuesday_order(2, client, articles);
-    std::cout << "Commande avec réduction du mardi: ";
-    print_total_price(tuesday_order); // Attendu: 990€ (10% de réduction si mardi)
+    std::cout << "Commande avec réduction du mardi: " << std::endl;
+    std::cout << tuesday_order << std::endl;
 
     PackageReductionDiscountCommand big_order(3, client, articles);
-    std::cout << "Commande avec réduction pour total > 150€: ";
-    print_total_price(big_order); // Attendu: 1090€ (-10€ si total > 150€)
+    std::cout << "Commande avec réduction pour total > 150€: " << std::endl;
+    std::cout << big_order << std::endl;
 
     Command* polymorphic_order = &tuesday_order;
-    std::cout << "Commande polymorphique (réduction du mardi si applicable): ";
-    print_total_price(*polymorphic_order);
+    std::cout << "Commande polymorphique (réduction du mardi si applicable): " << std::endl;
+    std::cout << *polymorphic_order << std::endl;
 
     return 0;
 }
